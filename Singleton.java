5cfbbd861432e4990ff4e455720b7bd2136a8edd
roy ash <roy.ash@moovit.com>
2024-03-06 15:42:54 +0200

    changed the way I publish to Maven Central + maintenance
    Signed-off-by: roy ash <roy.ash@moovit.com>

--- Singleton.java
+++ Singleton.java
@@ -2,25 +2,20 @@
 //
 //import java.util.function.Supplier;
 //
-//public class Singleton<T> implements Supplier<T>
-//{
+//public class Singleton<T> implements Supplier<T> {
 //	private final Supplier<T> supplier;
 //	private T result = null;
 //	private boolean isSolved;
 //
-//	public Singleton(Supplier<T> supplier)
-//	{
+//	public Singleton(Supplier<T> supplier) {
 //		this.supplier = supplier;
 //	}
 //
 //	@Override
-//	public final T get()
-//	{
+//	public final T get() {
 //		if (!isSolved)
-//			synchronized (this)
-//			{
-//				if (!isSolved)
-//				{
+//			synchronized (this) {
+//				if (!isSolved) {
 //					isSolved = true;
 //					return result = supplier.get();
 //				}
--- src/main/java/io/github/ashr123/timeMeasurement/Result.java
+++ src/main/java/io/github/ashr123/timeMeasurement/Result.java
@@ -3,24 +3,20 @@ package io.github.ashr123.timeMeasurement;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class Result<T>
-{
+public class Result<T> {
 	private final T result;
 	private final long timeTaken;
 	private final boolean isContainsResult;
 
-	Result(final long startTime, final long endTime)
-	{
+	Result(final long startTime, final long endTime) {
 		this(startTime, null, endTime, false);
 	}
 
-	Result(final long startTime, final T result, final long endTime)
-	{
+	Result(final long startTime, final T result, final long endTime) {
 		this(startTime, result, endTime, true);
 	}
 
-	private Result(final long startTime, final T result, final long endTime, final boolean isContainsResult)
-	{
+	private Result(final long startTime, final T result, final long endTime, final boolean isContainsResult) {
 		this.result = result;
 		this.timeTaken = endTime - startTime;
 		this.isContainsResult = isContainsResult;
@@ -29,8 +25,7 @@ public class Result<T>
 	/**
 	 * @return the result of the given function, if the function didn't return any result, {@link NoSuchElementException} is thrown
 	 */
-	public T getResult()
-	{
+	public T getResult() {
 		if (isContainsResult)
 			return result;
 		throw new NoSuchElementException("No result is present");
@@ -40,8 +35,7 @@ public class Result<T>
 	 * @return the time took for the code to complete by default timescale of milliseconds
 	 * @see Result#getTimeTaken(TimeScales)
 	 */
-	public double getTimeTaken()
-	{
+	public double getTimeTaken() {
 		return getTimeTaken(TimeScales.MILLISECONDS);
 	}
 
@@ -50,14 +44,12 @@ public class Result<T>
 	 * @return the time took for the code to complete by the given timescale
 	 * @see Result#getTimeTaken()
 	 */
-	public double getTimeTaken(TimeScales timeScales)
-	{
+	public double getTimeTaken(TimeScales timeScales) {
 		return timeTaken / timeScales.getScale();
 	}
 
 	@Override
-	public boolean equals(Object o)
-	{
+	public boolean equals(Object o) {
 		return this == o ||
 				o instanceof Result<?> result &&
 						timeTaken == result.timeTaken &&
@@ -66,14 +58,12 @@ public class Result<T>
 	}
 
 	@Override
-	public int hashCode()
-	{
+	public int hashCode() {
 		return Objects.hash(result, timeTaken, isContainsResult);
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "Result{" +
 				"result=" + result +
 				", timeTaken=" + timeTaken +
